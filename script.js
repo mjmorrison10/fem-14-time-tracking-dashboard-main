2a30cbb1d693b6aa6b1f26ee1fd935bc9560bcb3
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-10-31 18:32:30 -0700

    init

---
+++ script.js
@@ -0,0 +1,50 @@
+const daily = document.querySelector(".name-daily");
+const dailyCard = document.querySelectorAll(".daily");
+
+const weekly = document.querySelector(".name-weekly");
+const weeklyCard = document.querySelectorAll(".weekly");
+
+const monthly = document.querySelector(".name-monthly");
+const monthlyCard = document.querySelectorAll(".monthly");
+
+daily.addEventListener("click", () => {
+  dailyCard.forEach((card) => {
+    card.classList.add("show");
+  });
+
+  weeklyCard.forEach((card) => {
+    card.classList.remove("show");
+  });
+
+  monthlyCard.forEach((card) => {
+    card.classList.remove("show");
+  });
+});
+
+weekly.addEventListener("click", () => {
+  dailyCard.forEach((card) => {
+    card.classList.remove("show");
+  });
+
+  weeklyCard.forEach((card) => {
+    card.classList.add("show");
+  });
+
+  monthlyCard.forEach((card) => {
+    card.classList.remove("show");
+  });
+});
+
+monthly.addEventListener("click", () => {
+  dailyCard.forEach((card) => {
+    card.classList.remove("show");
+  });
+
+  weeklyCard.forEach((card) => {
+    card.classList.remove("show");
+  });
+
+  monthlyCard.forEach((card) => {
+    card.classList.add("show");
+  });
+});
